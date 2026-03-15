163939d0b478d504d7862e15500a71aa67846714
K1-R1 <77465250+K1-R1@users.noreply.github.com>
2026-03-15 14:52:32 +0000

    test: add integration test for `smoosh --docs` functionality in a git repository.

--- Formula/smoosh.rb
+++ Formula/smoosh.rb
@@ -11,5 +11,13 @@ class Smoosh < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/smoosh --version")
+    system "git", "init", testpath/"repo"
+    system "git", "-C", testpath/"repo", "config", "user.email", "test@brew.sh"
+    system "git", "-C", testpath/"repo", "config", "user.name", "Test"
+    (testpath/"repo/README.md").write("# Hello\n\nSmoke test.\n")
+    system "git", "-C", testpath/"repo", "add", "README.md"
+    system "git", "-C", testpath/"repo", "commit", "-m", "init"
+    system bin/"smoosh", "--no-interactive", "--docs", testpath/"repo"
+    assert_predicate testpath/"repo/_smooshes", :directory?
   end
 end
